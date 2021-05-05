c2ce40b4dce3cb1195563741e78b6dd94a6d4885
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-04 22:29:11 -0400

    Added comments for steps

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,12 @@ public class App {
         f = new XCubed();
         rs.setFunction(f);
         System.out.println(rs.integrate(1, 2));
+
+        // Find the volume of the region bounded by y = x^2, x = 2 and the y = 0 about the x-axis
+        // \pi\in_{0}^{2}(x^2)^2 dx
+
+        // 1. Find the region (2d area).
+        // 2. Find the limits of integration.
+        // 3. Integrate, don't forget pi!
     }
 }
