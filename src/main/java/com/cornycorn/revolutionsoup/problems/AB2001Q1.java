ca2a45b842ef59caca8510eb6502f014864f5e93
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-11 10:41:55 -0400

    Began part (a) annotations

--- src/main/java/com/cornycorn/revolutionsoup/problems/AB2001Q1.java
+++ src/main/java/com/cornycorn/revolutionsoup/problems/AB2001Q1.java
@@ -10,18 +10,19 @@ public class AB2001Q1 {
     private static final String A = "(a) FInd the area of R.";
     private static final String B = "(b) Find the area of S.";
     private static final String C = "(c) Find the volume of the solid generated when S is revolved about the x-axis.";
+Be
+    private static final String INVALID_PROBLEM = " is not a valid problem.";
+    private static final String DIVIDER = "---------------------------------------------------------------";
 
     public void solve() {
-        System.out.println(NAME);
-        System.out.println(PROBLEM + "\n");
-        System.out.println(A);
-        System.out.println(B);
-        System.out.println(C);
-
         boolean exit = false;
         while (!exit) {
-            System.out.println("---------------------------------------------------------------\n" +
-                    "Solve (a), (b), or (c)? (q to quit)");
+            System.out.println(DIVIDER + DIVIDER + "\n" + NAME);
+            System.out.println(PROBLEM + "\n");
+            System.out.println(A);
+            System.out.println(B);
+            System.out.println(C);
+            System.out.println(DIVIDER + "\n" + "Solve (a), (b), or (c)? (q to quit)");
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
 
@@ -30,12 +31,21 @@ public class AB2001Q1 {
                 case "b" -> solveB();
                 case "c" -> solveC();
                 case "q" -> exit = true;
+                default -> System.out.println(input + INVALID_PROBLEM);
             }
         }
     }
 
     public void solveA() {
-        System.out.println("a");
+        System.out.println("The problem gives us the region that R is bounded to. The solution is as simple as " +
+                "calculating the area between two curves.\n");
+        System.out.println("You should first notice that the region R has three total 'things' binding it: the " +
+                "x-axis, y = tan x, and y = 2 - x^3. We should also notice that half way through R, the top curve " +
+                "changes, oh no! This means that we will need to take the sum of two integrals.\n");
+        System.out.println("Our first integral will have a top function of y = tan x and bottom function of y = 0. " +
+                "Our limits of integration are from 0 to where the top curve changes from y = tan x to y = 2 - x^3. " +
+                "The upper limit of integration can be calculated by finding where the two top functions intersect. " +
+                "Here, that is where \n\ttan x = 2 - x^3\n");
     }
 
     public void solveB() {
