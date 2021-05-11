a460a9b3d421cf458de4ad0210011796d9b44876
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-11 07:18:06 -0400

    Added integration test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,17 @@
 public class App {
     public static void main(String[] args) {
         RevolutionSoup rs = new RevolutionSoup(new SinX(), new XAxis(), 10000000);
+
+        // Test 1
+        System.out.println(rs.integrate(0, (2 * Math.PI), new SinX()));
+        // OUT: 8.820740479912989E-14
+
+        // Maybe we should add a rounding function that will take these super
+        // small values and just return 0? For an increased value of N, it
+        // returns an even smaller non-zero value.
+
+        // Test 2
         System.out.println(rs.revolution(0, (2*Math.PI), 0));
-
-
-        // Find the volume of the region bounded by y = x^2, x = 2 and y = 0 about the x-axis
-        // \pi\in_{0}^{2}(x^2)^2 dx
-
-        // 1. Find the region (2d area).
-        // 2. Find the limits of integration.
-        // 3. Integrate, don't forget pi!
+        // OUT: 9.869604401090964
     }
 }
