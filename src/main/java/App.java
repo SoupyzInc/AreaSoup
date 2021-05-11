3609375a72b64ebcbbd6b237c4259f2d18800922
jojongx <jojongx@gmail.com>
2021-05-10 21:25:03 -0400

    Tested new sin(x) and y = 0 functions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,24 +2,11 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        RevolutionSoup rs = new RevolutionSoup(new XCubed(), new XSquared(), 10000000);
-        System.out.println(rs.revolution(0, 1, 3));
+        RevolutionSoup rs = new RevolutionSoup(new SinX(), new XAxis(), 10000000);
+        System.out.println(rs.revolution(0, (2*Math.PI), 0));
 
-//        JFrame f = new JFrame();
-//
-//        String[] problems = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
-//
-//        //Create the combo box, select item at index 4.
-//        //Indices start at 0, so 4 specifies the pig.
-//        JComboBox problemList = new JComboBox(problems);
-//        problemList.setSelectedIndex(4);
-//        problemList.addActionListener(f);
-//
-//        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-//        f.setLayout(null);
-//        f.setVisible(true);
 
-        // Find the volume of the region bounded by y = x^2, x = 2 and the y = 0 about the x-axis
+        // Find the volume of the region bounded by y = x^2, x = 2 and y = 0 about the x-axis
         // \pi\in_{0}^{2}(x^2)^2 dx
 
         // 1. Find the region (2d area).
