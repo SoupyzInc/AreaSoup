64c85c5ba6f8e3d49c5b124d9cbea3e745416a20
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-12 21:06:36 -0400

    Preparing problem classes to work with Java Swing

--- src/main/java/com/cornycorn/revolutionsoup/problems/AB2001Q1.java
+++ src/main/java/com/cornycorn/revolutionsoup/problems/AB2001Q1.java
@@ -14,32 +14,26 @@ public class AB2001Q1 {
     private static final String B = "(b) Find the area of S.";
     private static final String C = "(c) Find the volume of the solid generated when S is revolved about the x-axis.";
 
+    private static final int NUMBER_OF_PARTS = 3;
+
     private static final String INVALID_PROBLEM = " is not a valid problem.";
     private static final String DIVIDER = "---------------------------------------------------------------";
 
     /**
      * Begins solving this problem.
      */
-    public static void solve() {
-        boolean exit = false;
-        while (!exit) {
-            System.out.println(DIVIDER + DIVIDER + "\n" + NAME);
-            System.out.println(PROBLEM + "\n");
-            System.out.println(A);
-            System.out.println(B);
-            System.out.println(C);
-            System.out.println(DIVIDER + "\n" + "Solve (a), (b), or (c)? (q to quit)");
-            Scanner in = new Scanner(System.in);
-            String input = in.nextLine();
+    public static String solve() {
+        return NAME + "\n" + PROBLEM + "\n" + A + "\n" +  B + "\n" +  C;
+//            Scanner in = new Scanner(System.in);
+//            String input = in.nextLine();
 
-            switch (input) {
-                case "a" -> solveA();
-                case "b" -> solveB();
-                case "c" -> solveC();
-                case "q" -> exit = true;
-                default -> System.out.println(input + INVALID_PROBLEM);
-            }
-        }
+//            switch (input) {
+//                case "a" -> solveA();
+//                case "b" -> solveB();
+//                case "c" -> solveC();
+//                case "q" -> exit = true;
+//                default -> System.out.println(input + INVALID_PROBLEM);
+//            }
     }
 
     /**
