f28e67fe6dc3ca54a40a806e87ed16cec77a60a6
Matthew Okashita <okashita.matthew@gmail.com>
2021-05-14 08:02:07 -0400

    Added square root function

---
+++ src/main/java/com/cornycorn/revolutionsoup/functions/SquareRootX.java
@@ -0,0 +1,8 @@
+package com.cornycorn.revolutionsoup.functions;
+
+public class SquareRootX implements Function {
+    @Override
+    public double f(double x) {
+        return Math.sqrt(x);
+    }
+}
